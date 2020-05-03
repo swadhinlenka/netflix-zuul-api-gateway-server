ca431147cf0ff41b5254393feee8ab777c0a81a9
swadhin <1985swadhin@gmail.com>
2020-05-03 13:15:14 +0530

    first commit

---
+++ src/main/java/com/magic30minutes/microservices/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,24 @@
+package com.magic30minutes.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
